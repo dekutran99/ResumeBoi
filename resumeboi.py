05c11b98858408b2957ff7f74eaa142ba7e83221
janie wang <janiewang26@gmail.com>
2020-02-13 14:59:54 -0800

    scrolling

--- resumeboi.py
+++ resumeboi.py
@@ -1,11 +1,13 @@
-import requests
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 import os.path
 
 
-driver = webdriver.Chrome(r'C:\Users\huytran\Projects\ResumeBoi\chromedriver.exe')
+driver = webdriver.Firefox(executable_path = r'C:\Users\janie\Documents\Projects\resumeBoi\geckodriver.exe')
 
 driver.implicitly_wait(60000)
+
+driver.maximize_window()
 
 driver.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
 
@@ -13,6 +15,8 @@ email_path = '//*[@id="username"]'
 password_path = '//*[@id="password"]'
 signin_path = '//*[@id="app__container"]/main/div/form/div[3]/button'
 jobs_path = '//*[@id="jobs-nav-item"]/a'
+jobList_path = '//*[@class="job-card-search__link-wrapper.js-focusable.disabled.ember-view"]'
+nextjob_path = '//*[@class="job-card-search__link-wrapper.js-focusable.disabled.ember-view"]/following-sibling::p'
 
 email_text = driver.find_element_by_xpath(email_path)
 email_text.send_keys('arnold199926@gmail.com')
@@ -34,7 +38,15 @@ search_button = driver.find_element_by_class_name('jobs-search-box__submit-butto
 search_button.click()
 
 job_list = driver.find_elements_by_class_name("job-card-search__link-wrapper.js-focusable.disabled.ember-view")
+# job_list = driver.find_elements_by_xpath(nextjob_path)
 
 # deprecated
 for job in job_list:
     job.click()
+    driver.execute_script("arguments[0].scrollIntoView();", job )
+    job_list.append(driver.find_element_by_class_name("job-card-search__link-wrapper.js-focusable.disabled.ember-view"))
+
+# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+# page_bottom = driver.find_element_by_class_name("jobs-search-two-pane__pagination")
+
+# ActionChains(driver).move_to_element(page_bottom).perform()
