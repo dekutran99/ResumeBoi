cfcd7adc7f95ca506ac7b373b19f577f90da86b3
mrhuytran <dekutran@gmail.com>
2020-02-13 02:32:21 -0800

    ye boiiiiiiiiii

---
+++ resumeboi.py
@@ -0,0 +1,40 @@
+import requests
+from selenium import webdriver
+import os.path
+
+
+driver = webdriver.Chrome(r'C:\Users\huytran\Projects\ResumeBoi\chromedriver.exe')
+
+driver.implicitly_wait(60000)
+
+driver.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
+
+email_path = '//*[@id="username"]'
+password_path = '//*[@id="password"]'
+signin_path = '//*[@id="app__container"]/main/div/form/div[3]/button'
+jobs_path = '//*[@id="jobs-nav-item"]/a'
+
+email_text = driver.find_element_by_xpath(email_path)
+email_text.send_keys('arnold199926@gmail.com')
+
+password_text = driver.find_element_by_xpath(password_path)
+password_text.send_keys('Huy@123456')
+
+signin_button = driver.find_element_by_xpath(signin_path)
+signin_button.click()
+
+jobs_button = driver.find_element_by_xpath(jobs_path)
+jobs_button.click()
+
+search_jobs_texts = driver.find_elements_by_class_name('jobs-search-box__text-input')
+search_jobs_texts[0].send_keys('developer intern')
+
+
+search_button = driver.find_element_by_class_name('jobs-search-box__submit-button.artdeco-button.artdeco-button--3.ml2')
+search_button.click()
+
+job_list = driver.find_elements_by_class_name("job-card-search__link-wrapper.js-focusable.disabled.ember-view")
+
+# deprecated
+for job in job_list:
+    job.click()
